3f082fff9f2f133e2b59fbf59e4270af6fd88294
Joanne <jl7105@nyu.edu>
2022-06-26 13:54:11 -0400

    Hand Class

--- blackjack.py
+++ blackjack.py
@@ -6,7 +6,7 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 
 # Dictionary:
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
-            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace': 11}
 
 
 # Single Card Class ===========================================
@@ -27,16 +27,50 @@ class Card:
 class Deck:
 
     def __init__(self):
-        self.all_cards = []
+        self.deck = []
 
         for suit in suits:
             for rank in ranks:
                 # Create the Card Object
                 created_card = Card(suit, rank)
-                self.all_cards.append(created_card)
+                self.deck.append(created_card)
     
     def shuffle(self):
-        random.shuffle(self.all_cards)
+        random.shuffle(self.deck)
     
     def deal_one(self):
-        return self.all_cards.pop()
+        return self.deck.pop()
+
+
+# Hand Class ===================================================
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += card.value
+    
+    def adjust_for_ace(self):
+        # If a hand's value exceeds 21 but it contains an Ace, we can reduce the Ace's value from 11 to 1 and continue playing.
+        while self.value > 21 and self.aces:
+            # Make the ace a 1 instead of 11:
+            self.value -= 10
+            self.aces -= 1
+
+
+# Setup New Game =================================================
+new_deck = Deck()
+new_deck.shuffle()
+
+human_player = Hand()
+computer_player = Hand()
+
+
+# Play the Game =================================================
+playing = True
+
+while playing:
+    pass
