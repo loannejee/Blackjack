8de416ecfe493c5c5ae103cc7fa44c7c33d9beee
Joanne <jl7105@nyu.edu>
2022-06-25 22:46:20 -0400

    Single Card Class

--- blackjack.py
+++ blackjack.py
@@ -5,3 +5,16 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 # Dictionary:
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
             'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+# Single Card Class ===========================================
+class Card:
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        # Gives integer:
+        self.value = values[rank]
+    
+    # What do I want to return when I make an instance?
+    def __str__(self):
+        return self.rank + " of " + self.suit
